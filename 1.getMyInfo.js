525ae6e92911ea32dbae08657aa541186c6e4b99
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2022-12-02 19:28:04 +0500

    xurshid

---
+++ 1.getMyInfo.js
@@ -0,0 +1,14 @@
+function names(name) {
+    console.log("mening ismim" , name);
+    
+}
+names("xurshid")
+
+
+
+function getMyInfo(name, age, job) {
+    return`mening ismim ${name}, mening yoshim ${age}, mening kasbim ${job}`
+    
+}
+
+console.log (getMyInfo("Xurshid",17,"csgo"));
