44936920fd2f5e2ece7185afdb4d68c20e4c7bdf
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2022-12-02 21:01:47 +0500

    salom

---
+++ 1-12-2022/index.js
@@ -0,0 +1,29 @@
+function addition(a ){
+    return a + 1
+}
+console.log(addition(1));
+
+
+
+function triArea(a,b) {
+    return a*b/2
+}
+console.log(triArea(10,10));
+
+
+
+
+
+
+function convert(a ){
+    return a * 60
+}
+console.log(convert(-1));
+
+
+
+
+function addition1(a,d){
+    return a+d
+}
+console.log(addition1(-3,-6));
---
+++ 2-12-2022/1.getMyInfo.js
@@ -0,0 +1,57 @@
+// function myNames(name) {
+//     console.log("mening ismim" , name);
+    
+// }
+// myNames("xurshid")
+
+
+
+// function getMyInfo(name, age, job) {
+//     return`mening ismim ${name}, mening yoshim ${age}, mening kasbim ${job}`
+    
+// }
+
+// console.log (getMyInfo("Xurshid",17,"csgo"));
+
+
+
+
+// addition(0) ➞ 1
+
+// addition(9) ➞ 10
+
+// addition(-3) ➞ -2
+
+
+
+function addition(a ){
+    return a + 1
+}
+console.log(addition(1));
+
+
+
+
+function triArea(a,b) {
+    return a*b/2
+}
+console.log(triArea(10,10));
+
+
+
+
+
+
+function convert(a ){
+    return a * 60
+}
+console.log(convert(-1));
+
+
+
+
+function addition1(a,d){
+    return a+d
+}
+console.log(addition1(-3,-6));
+
